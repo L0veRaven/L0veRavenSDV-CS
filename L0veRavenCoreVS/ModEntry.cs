9cadb0fbf4b29906b521cd0d8c9d1eff86099049
YasmineRomero <l0ve.raven.stream@gmail.com>
2025-01-10 13:38:14 -0800

    Tried to make 6am-7am tokens work

--- L0veRavenCoreVS/ModEntry.cs
+++ L0veRavenCoreVS/ModEntry.cs
@@ -5,7 +5,7 @@ using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 
-namespace L0veRaven.CoreDLL
+namespace L0veRavenCore
 {
     /// <summary>The mod entry point.</summary>
     internal sealed class ModEntry : Mod
@@ -18,8 +18,126 @@ namespace L0veRaven.CoreDLL
         public override void Entry(IModHelper helper)
         {
             helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+
+            this.Helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
         }
 
+        private void GameLoop_GameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            var api = this.Helper.ModRegistry.GetApi<IContentPatcherAPI>("Pathoschild.ContentPatcher");
+
+            api.RegisterToken(this.ModManifest, "sixA", () =>
+            {
+                string sixAm = "600";
+
+                // save is loaded
+                if (Context.IsWorldReady)
+                    return new[] { sixAm };
+
+                // or save is currently loading
+                if (SaveGame.loaded?.player != null)
+                    return new[] { sixAm };
+
+                // no save loaded (e.g. on the title screen)
+                return new[] { sixAm };
+            });
+
+            api.RegisterToken(this.ModManifest, "sixtenA", () =>
+            {
+                string sixTenAm = "610";
+
+                // save is loaded
+                if (Context.IsWorldReady)
+                    return new[] { sixTenAm };
+
+                // or save is currently loading
+                if (SaveGame.loaded?.player != null)
+                    return new[] { sixTenAm };
+
+                // no save loaded (e.g. on the title screen)
+                return new[] { sixTenAm };
+            });
+
+            api.RegisterToken(this.ModManifest, "sixtwentyA", () =>
+            {
+                string sixTwentyAm = "620";
+
+                // save is loaded
+                if (Context.IsWorldReady)
+                    return new[] { sixTwentyAm };
+
+                // or save is currently loading
+                if (SaveGame.loaded?.player != null)
+                    return new[] { sixTwentyAm };
+
+                // no save loaded (e.g. on the title screen)
+                return new[] { sixTwentyAm };
+            });
+
+            api.RegisterToken(this.ModManifest, "sixthirtyA", () =>
+            {
+                string sixThirtyAm = "630";
+
+                // save is loaded
+                if (Context.IsWorldReady)
+                    return new[] { sixThirtyAm };
+
+                // or save is currently loading
+                if (SaveGame.loaded?.player != null)
+                    return new[] { sixThirtyAm };
+
+                // no save loaded (e.g. on the title screen)
+                return new[] { sixThirtyAm };
+            });
+
+            api.RegisterToken(this.ModManifest, "sixfortyA", () =>
+            {
+                string sixFortyAm = "640";
+
+                // save is loaded
+                if (Context.IsWorldReady)
+                    return new[] { sixFortyAm };
+
+                // or save is currently loading
+                if (SaveGame.loaded?.player != null)
+                    return new[] { sixFortyAm };
+
+                // no save loaded (e.g. on the title screen)
+                return new[] { sixFortyAm };
+            });
+
+            api.RegisterToken(this.ModManifest, "sixfiftyA", () =>
+            {
+                string sixFiftyAm = "650";
+
+                // save is loaded
+                if (Context.IsWorldReady)
+                    return new[] { sixFiftyAm };
+
+                // or save is currently loading
+                if (SaveGame.loaded?.player != null)
+                    return new[] { sixFiftyAm };
+
+                // no save loaded (e.g. on the title screen)
+                return new[] { sixFiftyAm };
+            });
+
+            api.RegisterToken(this.ModManifest, "sevenA", () =>
+            {
+                string sevenAm = "700";
+
+                // save is loaded
+                if (Context.IsWorldReady)
+                    return new[] { sevenAm };
+
+                // or save is currently loading
+                if (SaveGame.loaded?.player != null)
+                    return new[] { sevenAm };
+
+                // no save loaded (e.g. on the title screen)
+                return new[] { sevenAm };
+            });
+        }
 
         /*********
         ** Private methods
--- L0veRavenCoreVS/obj/Debug/net6.0/L0veRavenCore.AssemblyInfo.cs
+++ L0veRavenCoreVS/obj/Debug/net6.0/L0veRavenCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("L0veRavenCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c4b6824e7ff0ce182cb9545edeb071254abbdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9a57c50be4bd7a56d2ff402710ecb34ecf2cc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("L0veRavenCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("L0veRavenCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
