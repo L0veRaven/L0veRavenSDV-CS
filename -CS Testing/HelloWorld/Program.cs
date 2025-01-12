9525bc55ab773557ab9f47713f90fd035ce61a66
YasmineRomero <l0ve.raven.stream@gmail.com>
2025-01-11 17:01:36 -0800

    CS Learning Files Added

---
+++ -CS Testing/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;//Namespace
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        //<summary>Void means the method will not be return any values</summary>
+        //<summary>Main is the method</summary>
+        static void Main(string[] args)//Method
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
